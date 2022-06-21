30a571af12bfd4f7dd45da63d7b37b5d26cbcc3a
jithamanyu <sjithamanyu@gmail.com>
2022-06-22 01:24:17 +0530

    drawing lines second draft

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -5,6 +5,7 @@ import "../styles/style2.css";
 function Canvas(props) {
   var st_x,st_y;
   var points=[]
+  var point_draw=[]
   const setup = (p5, parent) => {
 
     p5.createCanvas(600, 600).parent(parent);
@@ -12,9 +13,23 @@ function Canvas(props) {
     p5.stroke(0,0,0)
   };
   const draw = (p5) => {
-    //if (p5.mouseIsPressed === true) {
-    //p5.line(p5.mouseX, p5.mouseY, p5.pmouseX, p5.pmouseY);
-  //}
+    p5.beginShape(p5.LINES);
+    for(let i=0;i<point_draw.length;i++){
+      
+    p5.vertex(point_draw[i][0][0],point_draw[i][0][1])
+    p5.vertex(point_draw[i][1][0],point_draw[i][1][1])
+    
+    }
+    p5.endShape()
+  /*if (p5.mouseIsPressed === true) {
+    points.push([p5.mouseX,p5.mouseY])
+    p5.beginShape(p5.LINES);
+    p5.vertex(points[0][0],points[0][1])
+    p5.vertex(points[1][0],points[1][1])
+    p5.endShape()
+    console.log(points)
+    points.pop()
+  }*/
   };
   
    const mp = (p5,event) =>{
@@ -23,27 +38,40 @@ function Canvas(props) {
     st_x=p5.mouseX;
     st_y=p5.mouseY;
     points.push([st_x,st_y])
-    console.log(points)
+    //console.log(points)
     }
   }
   const md=(p5,event)=>{
     if(props.drawState==="Line"){
     points.push([p5.mouseX,p5.mouseY])
     //console.log(event)
+    
+    p5.background(255, 255, 255);
     p5.beginShape(p5.LINES);
     p5.vertex(points[0][0],points[0][1])
     p5.vertex(points[1][0],points[1][1])
     p5.endShape()
-    console.log(points)
+    //console.log(points)
     points.pop()
     }
   }
   const mr=(p5,event)=>{
     if(props.drawState==="Line"){
     //console.log(event)
-    p5.line(points[0][1],points[0][1],p5.mouseX,p5.mouseY)
-    points=[]
+    //p5.line(points[0][1],points[0][1],p5.mouseX,p5.mouseY)
+    points.push([p5.mouseX,p5.mouseY])
     console.log(points)
+    point_draw.push(points)
+    points=[]
+    console.log(point_draw)
+    p5.beginShape(p5.LINES);
+    for(let i=0;i<point_draw.length;i++){
+      
+    p5.vertex(point_draw[i][0][0],point_draw[i][0][1])
+    p5.vertex(point_draw[i][1][0],point_draw[i][1][1])
+    
+    }
+    p5.endShape()
     }
   }
 
