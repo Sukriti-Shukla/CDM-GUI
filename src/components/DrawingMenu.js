808237e625aad44760c989e76982f2bd4c0f28d6
Sukriti Shukla <sukritishukla0507@gmail.com>
2022-06-23 21:26:14 +0530

    change in top navbar onClick

--- src/components/DrawingMenu.js
+++ src/components/DrawingMenu.js
@@ -7,45 +7,74 @@ export const UserContext = createContext();
 function DrawingMenu(props) {
   const [draw, setDraw] = useState("line");
   const handleClickLine = () => {
+    document.getElementById("line-btn").classList.add("active");
+    document.getElementById("circle-btn").classList.remove("active");
+    document.getElementById("rect-btn").classList.remove("active");
+    document.getElementById("point-btn").classList.remove("active");
+    document.getElementById("reset-btn").classList.remove("active");
     setDraw("Line");
     console.log(draw);
   };
   const handleClickCircle = () => {
+    document.getElementById("circle-btn").classList.add("active");
+    document.getElementById("line-btn").classList.remove("active");
+    document.getElementById("rect-btn").classList.remove("active");
+    document.getElementById("point-btn").classList.remove("active");
+    document.getElementById("reset-btn").classList.remove("active");
     setDraw("Circle");
     console.log(draw);
   };
   const handleClickRectangle = () => {
+    document.getElementById("rect-btn").classList.add("active");
+    document.getElementById("line-btn").classList.remove("active");
+    document.getElementById("circle-btn").classList.remove("active");
+    document.getElementById("point-btn").classList.remove("active");
+    document.getElementById("reset-btn").classList.remove("active");
     setDraw("Rectangle");
     console.log(draw);
   };
   const handleClickPoint = () => {
+    document.getElementById("point-btn").classList.add("active");
+    document.getElementById("line-btn").classList.remove("active");
+    document.getElementById("circle-btn").classList.remove("active");
+    document.getElementById("rect-btn").classList.remove("active");
+    document.getElementById("reset-btn").classList.remove("active");
     setDraw("Point");
     console.log(draw);
   };
   const handleClickReset = () => {
+    document.getElementById("reset-btn").classList.add("active");
+    document.getElementById("line-btn").classList.remove("active");
+    document.getElementById("circle-btn").classList.remove("active");
+    document.getElementById("point-btn").classList.remove("active");
+    document.getElementById("rect-btn").classList.remove("active");
     setDraw("Reset");
     console.log(draw);
   };
   return (
     <div class="topnav">
-      <a class="active" href="#home">
+      <a class="active2" href="#home">
         Tools
       </a>
-      <a href="#line" onClick={handleClickLine}>
+      <a href="#line" id="line-btn" onClick={handleClickLine}>
         Line
       </a>
-      <a href="#circle" onClick={handleClickCircle}>
+      <a href="#circle" id="circle-btn" onClick={handleClickCircle}>
         Circle
       </a>
-      <a href="#ellipse">Ellipse</a>
-      <a href="#rect" onClick={handleClickRectangle}>
+      {/* <a href="#ellipse" id="ellipse-btn">
+        Ellipse
+      </a> */}
+      <a href="#rect" id="rect-btn" onClick={handleClickRectangle}>
         Rectangle
       </a>
-      <a href="#curve">Curves</a>
-      <a href="#point" onClick={handleClickPoint}>
+      {/* <a href="#curve" id="curve-btn">
+        Curves
+      </a> */}
+      <a href="#point" id="point-btn" onClick={handleClickPoint}>
         Point
       </a>
-      <a href="#reset" onClick={handleClickReset}>
+      <a href="#reset" id="reset-btn" onClick={handleClickReset}>
         Reset
       </a>
       <UserContext.Provider value={draw}>{props.children}</UserContext.Provider>
