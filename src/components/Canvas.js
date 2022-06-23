b5b0edd3ef3ee072c3db6abe3c8e9d6e1cb39715
Sukriti Shukla <sukritishukla0507@gmail.com>
2022-06-23 18:53:16 +0530

    made simpler code for lines

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -3,156 +3,41 @@ import Sketch from "react-p5";
 import "../styles/style2.css";
 import { useContext } from "react";
 import { UserContext } from "./DrawingMenu";
+// import p5Types from "p5";
+
+let drawShape;
+let points = [];
+
+const setup = (p5, parent) => {
+  p5.createCanvas(800, 500).parent(parent);
+  p5.background(255, 255, 255);
+  p5.stroke(0, 0, 0);
+};
+
+const mousePressed = (p5) => {
+  if (drawShape === "Line") {
+    points.push([p5.mouseX, p5.mouseY]);
+  }
+};
+
+const mouseReleased = (p5) => {
+  if (drawShape === "Line") {
+    console.log(points);
+    p5.line(points[0][0], points[0][1], p5.mouseX, p5.mouseY);
+    points = [];
+  }
+};
 
 function Canvas(props) {
-  // const drawShape = useContext(UserContext);
-  //   var st_x, st_y;
-  //   var points = [];
-  //   var point_draw = [];
-  //   const setup = (p5, parent) => {
-  //     p5.createCanvas(800, 500).parent(parent);
-  //     p5.background(255, 255, 255);
-  //     p5.stroke(0, 0, 0);
-  //   };
-  //   const draw = (p5) => {
-  //     p5.beginShape(p5.LINES);
-  //     for (let i = 0; i < point_draw.length; i++) {
-  //       p5.vertex(point_draw[i][0][0], point_draw[i][0][1]);
-  //       p5.vertex(point_draw[i][1][0], point_draw[i][1][1]);
-  //     }
-  //     p5.endShape();
-  //   };
-
-  //   const mp = (p5, event) => {
-  //     console.log(drawShape);
-  //     if (props.drawState === "Line") {
-  //       //console.log(event)~
-  //       st_x = p5.mouseX;
-  //       st_y = p5.mouseY;
-  //       points.push([st_x, st_y]);
-  //       console.log(points);
-  //     }
-  //   };
-  //   const md = (p5, event) => {
-  //     if (props.drawState === "Line") {
-  //       points.push([p5.mouseX, p5.mouseY]);
-  //       //console.log(event)
-
-  //       p5.background(255, 255, 255);
-  //       p5.beginShape(p5.LINES);
-  //       p5.vertex(points[0][0], points[0][1]);
-  //       p5.vertex(points[1][0], points[1][1]);
-  //       p5.endShape();
-  //       console.log(points);
-  //       points.pop();
-  //     }
-  //   };
-  //   const mr = (p5, event) => {
-  //     if (props.drawState === "Line") {
-  //       console.log(event);
-  //       p5.line(points[0][1], points[0][1], p5.mouseX, p5.mouseY);
-  //       //points.pop();
-  //       points.push([p5.mouseX, p5.mouseY]);
-  //       console.log(points);
-  //       point_draw.push(points);
-  //       points = [];
-  //       console.log(point_draw);
-  //       p5.beginShape(p5.LINES);
-  //       for (let i = 0; i < point_draw.length; i++) {
-  //         p5.vertex(point_draw[i][0][0], point_draw[i][0][1]);
-  //         p5.vertex(point_draw[i][1][0], point_draw[i][1][1]);
-  //       }
-  //       p5.endShape();
-  //     }
-  //   };
-
-  //   return (
-  //     <Sketch
-  //       setup={setup}
-  //       draw={draw}
-  //       mousePressed={mp}
-  //       mouseDragged={md}
-  //       mouseReleased={mr}
-  //     />
-  //   );
-  // }
-
-  // export default Canvas;
-  const drawShape = useContext(UserContext);
-  var st_x, st_y;
-  var points = [];
-  var point_draw = [];
-  const setup = (p5, parent) => {
-    p5.createCanvas(800, 500).parent(parent);
-    p5.background(255, 255, 255);
-    p5.stroke(0, 0, 0);
-  };
-  const draw = (p5) => {
-    p5.beginShape(p5.LINES);
-    for (let i = 0; i < point_draw.length; i++) {
-      p5.vertex(point_draw[i][0][0], point_draw[i][0][1]);
-      p5.vertex(point_draw[i][1][0], point_draw[i][1][1]);
-    }
-    p5.endShape();
-    /*if (p5.mouseIsPressed === true) {
-    points.push([p5.mouseX,p5.mouseY])
-    p5.beginShape(p5.LINES);
-    p5.vertex(points[0][0],points[0][1])
-    p5.vertex(points[1][0],points[1][1])
-    p5.endShape()
-    console.log(points)
-    points.pop()
-  }*/
-  };
-
-  const mp = (p5, event) => {
-    if (drawShape === "Line") {
-      //console.log(event)~
-      st_x = p5.mouseX;
-      st_y = p5.mouseY;
-      points.push([st_x, st_y]);
-      //console.log(points)
-    }
-  };
-  const md = (p5, event) => {
-    if (drawShape === "Line") {
-      points.push([p5.mouseX, p5.mouseY]);
-      //console.log(event)
-
-      p5.background(255, 255, 255);
-      p5.beginShape(p5.LINES);
-      p5.vertex(points[0][0], points[0][1]);
-      p5.vertex(points[1][0], points[1][1]);
-      p5.endShape();
-      //console.log(points)
-      points.pop();
-    }
-  };
-  const mr = (p5, event) => {
-    if (drawShape === "Line") {
-      //console.log(event)
-      //p5.line(points[0][1],points[0][1],p5.mouseX,p5.mouseY)
-      points.push([p5.mouseX, p5.mouseY]);
-      console.log(points);
-      point_draw.push(points);
-      points = [];
-      console.log(point_draw);
-      p5.beginShape(p5.LINES);
-      for (let i = 0; i < point_draw.length; i++) {
-        p5.vertex(point_draw[i][0][0], point_draw[i][0][1]);
-        p5.vertex(point_draw[i][1][0], point_draw[i][1][1]);
-      }
-      p5.endShape();
-    }
-  };
+  drawShape = useContext(UserContext);
 
   return (
     <Sketch
       setup={setup}
-      draw={draw}
-      mousePressed={mp}
-      mouseDragged={md}
-      mouseReleased={mr}
+      // draw={draw}
+      mousePressed={mousePressed}
+      // mouseDragged={md}
+      mouseReleased={mouseReleased}
     />
   );
 }
