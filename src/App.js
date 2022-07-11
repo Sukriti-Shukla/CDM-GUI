611af433ab44283a7c26aa641ae544e98dd2ede6
jithamanyu001 <82470772+jithamanyu001@users.noreply.github.com>
2022-07-12 01:02:32 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, useContext } from "react";
 import "../src/styles/style.css";
 import Canvas from "./components/Canvas";
 import { ChakraProvider } from "@chakra-ui/react";
 import Nav from "./components/Nav";
 import Sidebar from "./components/Sidebar";
 import Sketch from "react-p5";
+import { mouseX, mouseY } from "react-p5";
 import "../src/styles/style2.css";
+import { UserContext } from "./components/DrawingMenu";
 import DrawingMenu from "./components/DrawingMenu";
+import { Route, Routes } from "react-router-dom";
+import Result from "./components/Result";
+import Canvas2 from "./components/Canvas2";
+import Canvas3 from "./components/Canvas3";
+let dragX, dragY, moveX, moveY;
 
 function App() {
-  var state={
-    drawState:"Line"
-  }
   return (
     <div>
       <ChakraProvider>
         <Nav />
-        <DrawingMenu />
-        <Sidebar />
-        <div id="container2">
-          <Canvas drawState={state.drawState}/>
-        </div>
+        <DrawingMenu>
+          <Sidebar />
+          <div id="container2">
+            <Canvas2 />
+          </div>
+        </DrawingMenu>
       </ChakraProvider>
     </div>
   );
